7d4d9f8285643dbe720935ff4a317fab65b809d6
Pabl01802 <58702798+Pabl01802@users.noreply.github.com>
2022-03-13 15:27:48 +0100

    cały program 0 stylu bez generatora

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,92 @@
 import './App.sass'
+import questions from './questions.js'
+import { useState, useEffect } from 'react'
 
 function App() {
 
-  const questions = ['2', '32']
+  let [correctQ, setCorrectQ] = useState(0)
+  let [totalQ, setTotalQ] = useState(0)
+  let percentage = (correctQ/totalQ)*100
+
+  // useEffect(()=>{
+  //   setCorrectQ(correctQ++)
+  // }, [correctQ])
+  // useEffect(()=>{
+  //   setTotalQ(totalQ++)
+  // }, [totalQ])
+
+  const checkAnswer = (e) =>{
+    const content = e.target.innerText
+    const index = e.target.id
+    const div = document.querySelector(`.${e.target.className}`)
+    console.log(div)
+    if(questions[index].chance === 1){
+      if(content === questions[index].correct){
+        setCorrectQ(prevState => prevState + 1)
+        console.log('Prawidłowa odpowiedź')
+        e.target.style.backgroundColor = 'green'
+      }else{
+        console.log('Zła odpowiedź')
+        e.target.innerHTML+= ` &nbsp Prawidłowa odpowiedź: ${questions[index].correct}`
+        e.target.style.backgroundColor = 'red'
+      }
+      questions[index].chance--
+      setTotalQ(prevState => prevState + 1)
+      console.log(totalQ)
+    }else{
+      console.log('Szanse wykorzystane')
+    }
+  }
 
   return (
     <div className="App">
       <div className='top-app-wrapper'>
         Egzamin
       </div>
-      <div>
-        {questions.map(q => {
-          return <h1>{q}</h1>
+      <div className='top-bar-wrapper'>
+        <div className='top-bar-score'>
+          {/* {correctQ}/{totalQ} {percentage}% */}
+          { totalQ === 0 ? <h1>Brak odpowiedzi</h1> : <h1>{correctQ}/{totalQ} {Math.floor(percentage)}%</h1>}
+        </div>
+      </div>
+      <div className='app-wrapper'>
+        {
+        questions.map(q => {
+          return(
+            <>
+              <div className='question-wraper'>
+                <div className='question-title'>
+                  <span>{q.title}</span>
+                </div>
+                <div className='question-answers-wrapper'>
+                  <div 
+                  className='question-answer-content'
+                  onClick={checkAnswer}
+                  id={q.id}>
+                    <span>{q.answer1}</span>
+                  </div>
+                  <div 
+                  className='question-answer-content'
+                  onClick={checkAnswer}
+                  id={q.id}>
+                    <span>{q.answer2}</span>
+                  </div>
+                  <div 
+                  className='question-answer-content'
+                  onClick={checkAnswer}
+                  id={q.id}>
+                    <span>{q.answer3}</span>
+                  </div>
+                  <div 
+                  className='question-answer-content'
+                  onClick={checkAnswer}
+                  id={q.id}>
+                    {q.answer4}
+                  </div>
+                </div>
+              </div>
+            </>
+          )
         })}
       </div>
     </div>
---
+++ app/src/questions.js
@@ -0,0 +1,32 @@
+const questions = [{
+    id: '0',
+    title: '7+7',
+    answer1: '12',
+    answer2: '13',
+    answer3: '14',
+    answer4: '15',
+    correct: '14',
+    chance: 1
+},
+{   
+    id: '1',
+    title: '8+8',
+    answer1: '15',
+    answer2: '16',
+    answer3: '17',
+    answer4: '18',
+    correct: '16',
+    chance: 1
+},
+{
+    id: '2',
+    title: '9+9',
+    answer1: '18',
+    answer2: '19',
+    answer3: '20',
+    answer4: '21',
+    correct: '18',
+    chance: 1
+}];
+
+export default questions;
